7fd8dc41bc82e5e5c31ff202bff2a3d845dd99f1
TedZamarron <ted.zamarron@gmail.com>
2020-02-11 15:26:15 -0600

    commented out items in break_and_continue.js and while.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,15 +5,14 @@
 //     var userNum = parseFloat(prompt("Pick an odd number between 1 and 50."));
 // } while (isNaN(userNum) || userNum % 2 === 0);
 
-var userNum = parseFloat(prompt("Pick a number between 1 and 50"));
-console.log("Number to skip is: " + userNum);
-for (var i = 1; i <= 50; i++) {
-    if (i === userNum){
-        console.log("Yikes! Skipping number:" + userNum);
-    } else if (i % 2 === 0){
-
-    } else {
-        console.log(i);
-    }
-
-}
+// var userNum = parseFloat(prompt("Pick a number between 1 and 50"));
+// console.log("Number to skip is: " + userNum);
+// for (var i = 1; i <= 50; i++) {
+//     if (i === userNum){
+//         console.log("Yikes! Skipping number:" + userNum);
+//     } else if (i % 2 === 0){
+//
+//     } else {
+//         console.log(i);
+//     }
+// }
