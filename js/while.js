86ed9948e4406657e7c77c7d3b6dd94c20a62425
TedZamarron <ted.zamarron@gmail.com>
2020-02-11 11:54:21 -0600

    started loop exercises

---
+++ js/while.js
@@ -0,0 +1,18 @@
+"use strict";
+(function () {
+    //exercise 1
+    // var i = 2;
+    // while (i<66000){
+    //     console.log(i);
+    //     i*=2;
+    // }
+
+    //exercise 2
+    // This is how you get a random number between 50 and 100
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    // This expression will generate a random number between 1 and 5
+    Math.floor(Math.random() * 5) + 1;
+
+
+
+})();
