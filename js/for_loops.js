97753713abcced5ca08caf46b3b21d8f64a2e35b
TedZamarron <ted.zamarron@gmail.com>
2020-02-11 15:10:45 -0600

    completed loops exercise

---
+++ js/for_loops.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// function showMultiplicationTable(number) {
+//     for (var i = 1; i <= 10; i++){
+//         console.log(number + ' x ' + i + " = " + (number * i));
+//     }
+// }
+// showMultiplicationTable(7);
+
+
+// var randomNumber = function() {
+//     return Math.floor(Math.random() * 20) + 180;
+// };
+//
+//
+// for (var i = 1; i<=10;i++){
+//     var x = randomNumber();
+//     // console.log(x);
+//     if (x % 2 === 0){
+//         console.log(x + " is even");
+//     } else {
+//         console.log(x + " is odd");
+//     }
+// }
+
+
+
+// for(var i = 1; i<=9;i++){
+//     console.log(("" + i).repeat(i));
+// }
+
+
+// for (var i = 100;i >= 5;i-=5){
+//     console.log(i);
+// }
+
+
+
+
--- js/while.js
+++ js/while.js
@@ -1,5 +1,5 @@
 "use strict";
-(function () {
+
     //exercise 1
     // var i = 2;
     // while (i<66000){
@@ -9,10 +9,25 @@
 
     //exercise 2
     // This is how you get a random number between 50 and 100
-    var allCones = Math.floor(Math.random() * 50) + 50;
-    // This expression will generate a random number between 1 and 5
-    Math.floor(Math.random() * 5) + 1;
+    // // This expression will generate a random number between 1 and 5
+    // Math.floor(Math.random() * 5) + 1;
 
+    // var allCones = Math.floor(Math.random() * 50) + 50;
+    // var userCones = function(){
+    //     return Math.floor(Math.random() * 5) + 1;
+    //     };
+    //
+    // do{
+    //     var xSold = userCones();
+    //     console.log(allCones);
+    //     if (allCones>=xSold){
+    //         console.log(xSold + 'cones sold ...');
+    //         allCones-=xSold;
+    //     } else if (allCones === 0){
+    //         console.log("Yay! I sold them all!");
+    //         break;
+    //     } else {
+    //         console.log("Cannot sell you " + xSold + " I only have " + allCones + " ...");
+    //     }
+    // } while (allCones >= 0);
 
-
-})();
