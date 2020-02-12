060e60cdc8748de818d8b83b1ecb12ce1239e2b8
TedZamarron <ted.zamarron@gmail.com>
2020-02-12 08:48:26 -0600

    modified js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,16 +3,16 @@
 
 // do {
 //     var userNum = parseFloat(prompt("Pick an odd number between 1 and 50."));
-// } while (isNaN(userNum) || userNum % 2 === 0);
+// } while(isNaN(userNum) || userNum % 2 === 0);
+
+
 
 // var userNum = parseFloat(prompt("Pick a number between 1 and 50"));
 // console.log("Number to skip is: " + userNum);
-// for (var i = 1; i <= 50; i++) {
-//     if (i === userNum){
-//         console.log("Yikes! Skipping number:" + userNum);
-//     } else if (i % 2 === 0){
-//
-//     } else {
-//         console.log(i);
-//     }
+// for (var i = 1; i < 50; i+=2) {
+// 	if (i === userNum) {
+//         console.log("Yikes! Skipping number: " + userNum);
+//         continue;
+// 	}
+// 	console.log('Here is an odd number: ' + i);
 // }
--- js/while.js
+++ js/while.js
@@ -13,23 +13,23 @@
     // // This expression will generate a random number between 1 and 5
     // Math.floor(Math.random() * 5) + 1;
 
-    var allCones = Math.floor(Math.random() * 50) + 50;
-    var numCustomers = 0;
-
-    do{
-        var xSold = Math.floor(Math.random() * 5) + 1;
-        console.log(allCones);
-        if (allCones>=xSold){
-            console.log(xSold + ' cones sold ...');
-            allCones-=xSold;
-            numCustomers+=1;
-        } else {
-            console.log("Cannot sell you " + xSold + " I only have " + allCones + " ...");
-            numCustomers+=1;
-        }
-    } while (allCones !== 0);
-console.log("Yay! I sold them all!");
-console.log("Total customers today: " + numCustomers);
+//     var allCones = Math.floor(Math.random() * 50) + 50;
+//     var numCustomers = 0;
+//
+//     do{
+//         var xSold = Math.floor(Math.random() * 5) + 1;
+//         console.log(allCones);
+//         if (allCones>=xSold){
+//             console.log(xSold + ' cones sold ...');
+//             allCones-=xSold;
+//             numCustomers+=1;
+//         } else {
+//             console.log("Cannot sell you " + xSold + " I only have " + allCones + " ...");
+//             numCustomers+=1;
+//         }
+//     } while (allCones !== 0);
+// console.log("Yay! I sold them all!");
+// console.log("Total customers today: " + numCustomers);
 
 
 
