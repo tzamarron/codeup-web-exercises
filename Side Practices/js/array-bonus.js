4c33129e9a4f9617d90a382a7bbc888d6ae2903f
TedZamarron <ted.zamarron@gmail.com>
2020-02-12 17:46:39 -0600

    completed some of the second half of bonus

--- Side Practices/js/array-bonus.js
+++ Side Practices/js/array-bonus.js
@@ -2,18 +2,18 @@
     "use strict";
 
     //bonus 1
-    // var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
-    //
-    // function allIndexesOf(array,value){
-    //     var timesInArray = [];
-    //     for(var i = 0; i < array.length; i++){
-    //         if(array[i] === value){timesInArray.push(i)}
-    //     }
-    //     return timesInArray;
-    // }
-    // console.log(allIndexesOf(fruits, 'apple'));//[0,3]
-    // console.log(allIndexesOf(fruits, 'guava'));//[]
-    // console.log(allIndexesOf(fruits, 'pineapple'));//[4]
+    var fruits = ["apple", "banana", "orange", "apple", "pineapple"];
+
+    function allIndexesOf(array,value){
+        var timesInArray = [];
+        for(var i = 0; i < array.length; i++){
+            if(array[i] === value){timesInArray.push(i)}
+        }
+        return timesInArray;
+    }
+    console.log(allIndexesOf(fruits, 'apple'));//[0,3]
+    console.log(allIndexesOf(fruits, 'guava'));//[]
+    console.log(allIndexesOf(fruits, 'pineapple'));//[4]
 
 
     //Bonus 2
@@ -40,19 +40,23 @@
 
 
     // // Bonus4
-    function coinFlip(){
-        var toss =Math.random();
-        var flip = toss < 0.5 ? toss = Math.floor(toss) : toss = Math.ceil(toss);
-        return flip;
-    }
-    console.log(coinFlip());
+    // function coinFlip(){
+    //     // var toss =Math.random();
+    //     // var flip = toss < 0.5 ? toss = Math.floor(toss) : toss = Math.ceil(toss);
+    //     // return flip;
+    //     var array = [0, 1];
+    //     return Math.floor(Math.random() * array.length);
+    //
+    // }
+    // console.log(coinFlip());
 
 
     // Bonus 5
     // function twoDice() {
-    //     var dice1 = Math.floor(Math.random() * 7);
+    //     var sixSidedDice = [1, 2, 3, 4, 5, 6];
+    //     var dice1 = Math.floor(Math.random() * sixSidedDice.length);
     //     console.log(dice1);
-    //     var dice2 = Math.floor(Math.random() * 7);
+    //     var dice2 = Math.floor(Math.random() * sixSidedDice.length);
     //     console.log(dice2);
     //     return dice1 + dice2;
     // }
@@ -61,27 +65,35 @@
 
     // Bonus 6
     // function twentySideDie() {
-    //         return Math.floor(Math.random()*21);
+    //         // return Math.floor(Math.random()*21);
+    //         var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    //         return Math.floor(Math.random() * array.length);
     // }
     // console.log(twentySideDie());
 
 
     // Bonus 7
     // function twelveSidedDie() {
-    //     return Math.floor(Math.random()*13);
+    //     // return Math.floor(Math.random()*13);
+    //     var array = [1,2,3,4,5,6,7,8,9,10,11,12];
+    //     return Math.floor(Math.random() * array.length);
     // }
     // console.log(twelveSidedDie());
 
 
     // Bonus 8
     // function tetrahedron() {
-    //     return Math.floor(Math.random()*5);
+    //     // return Math.floor(Math.random()*5);
+    //     var array = [1,2,3,4];
+    //     return Math.floor(Math.random() * array.length);
     // }
     // console.log(tetrahedron());
 
     // Bonus 9
     // function rollDie() {
-    //     return Math.floor(Math.random()*7);
+    //     // return Math.floor(Math.random()*7);
+    //     var array = [1,2,3,4,5,6];
+    //     return Math.floor(Math.random() * array.length);
     // }
     // console.log(rollDie());
 
@@ -107,6 +119,29 @@
     // }
     // console.log(listOfDieRollsFromDieFunc(1, tetrahedron()));
 
+
+    // Bonus 2-1
+    // function first(array) {
+    //     return array[0];
+    // }
+
+
+    // Bonus 2-2
+    // function secondToLast(array) {
+    //     return array[array.length - 2];
+    // }
+
+
+    // Bonus 2-3
+    // function rest(array) {
+    //     return array.pop;
+    // }
+
+
+    // Bonus 2-4
+    function getLongestString(array) {
+        return array[0];
+    }
 
 
 })();
