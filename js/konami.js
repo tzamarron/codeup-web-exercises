56a80678e170d2e9e415796202eb6fc0ab7ed231
Ted Zamarron <ted.zamarron@gmail.com>
2020-02-29 11:28:08 -0600

    added new sprites and functionality

--- js/konami.js
+++ js/konami.js
@@ -1,42 +1,70 @@
 "use strict";
 
-// let inputs = [];
-// if(inputs.toString().indexOf("27") >= 0){inputs = []}
-//
-// let konamiCode = "38,38,40,40,37,39,37,39,66,65,13";
-//
-// $(document).keyup(function(input){
-//     inputs.push(input.keyCode);
-//     // console.log(inputs.toString());
-//
-//     if(inputs.toString().indexOf(konamiCode) >= 0 ) {
-//         alert("You have 30 lives!");
-//         // console.log(inputs);
-//         $("#BSOD").fadeOut("slow",function () {
-//             $(this).removeClass("slot")
-//         });
-//         inputs = [];
-//     }
-// });
 let userInput = [];
-
-let konamiCode = "38,38,40,40,37,39,37,39,66,65";
+// let konamiCode = "38,38,40,40,37,39,37,39,66,65,13";
+let sound = 0;
+let konamiCode = ["38","38","40","40","37","39","37","39","66","65","13"];
 
 $(document).keyup(function (input) {
     userInput.push(input.keyCode);
+    console.log(userInput);
 
-    if (userInput.indexOf("13") !== -1){
-        if (userInput.toString().indexOf(konamiCode) >= 0){
-
-        }
-    }
+    // if (userInput.toString().indexOf(konamiCode) >= 0){
+    //     $("#show2").fadeOut("fast").removeClass().addClass("screen");
+    //     $("#show1").fadeIn("fast");
+    //     $("div").unbind();
+    //     sound = 0;
+    //     userInput = [];
+    // }
 });
 
-let sound = 0;
-$("#screen").click(function () {
+$(".screen").click(function () {
     if (sound === 0){
-        $(".coin").get(0).play();
-        console.log("yes");
-        sound++
+        $(".coinSound").get(0).play();
+        // console.log("coin drop");
+        sound++;
+        $("#show1").fadeOut("slow");
+        $("#show2").fadeIn("fast");
+        $("#contraP1").hover(function () {
+            $("#show2").addClass("Contra")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#kirby").hover(function () {
+            $("#show2").addClass("KAdventure")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#kirbyOG").hover(function () {
+            $("#show2").addClass("KDreamland")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#mario").hover(function () {
+            $("#show2").addClass("Contra")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#bulba").hover(function () {
+            $("#show2").addClass("Contra")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#ogLink").hover(function () {
+            $("#show2").addClass("ZeldaOG")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#ootLink").hover(function () {
+            $("#show2").addClass("OOT")
+        },function(){
+            $("#show2").removeClass()
+        });
+        $("#botwLink").hover(function () {
+            $("#show2").addClass("BOTW")
+        },function(){
+            $("#show2").removeClass()
+        });
+
     }
 });
