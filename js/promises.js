3e97b35778bef353e22020ba52832f5c778d9dba
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-11 12:27:33 -0500

    modifications

---
+++ js/promises.js
@@ -0,0 +1,9 @@
+
+const wait = num => {
+    return new Promise( (res) => {
+        setTimeout(res,num);
+    })
+};
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
