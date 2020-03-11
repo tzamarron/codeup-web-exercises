8660c589cc8a395b32bae4a6e671b26b61f2b45d
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-10 20:34:58 -0500

    modifications

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,58 @@
+
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+users.filter(person => person.languages.length >= 3);
+
+//     Use .map to create an array of strings where each element is a user's email address
+users.map(person => person.email);
+
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+users.reduce((total,person) => {return total += person.yearsOfExperience},0);
+
+//     Use .reduce to get the longest email from the list of users.
+
+
+//     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+users.reduce((instructors, person) => {return instructors += (`${person.name}, `)},"Your instructors area: ");
+
+// Bonus
+// Use .reduce to get the unique list of languages from the list of users.
