6b7d5885b70e61acfbb572dedb35ced4bfe73696
Ted Zamarron <ted.zamarron@gmail.com>
2020-03-11 19:02:02 -0500

    completed promises.html

--- js/promises.js
+++ js/promises.js
@@ -9,35 +9,39 @@
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 const githubData = (user) => fetch(`https://api.github.com/users/${user}/events`,{headers: {'Authorization': githubToken}})
-    //receive data from API and turn to JSON
+    // Receive data from API and turn to JSON
     .then( response => {
         return response.json();
     })
-    //Take JSON and take only "PushEvents" aka push to origin
-    .then( (data) => {
-        return data.filter(item => item.type==="PushEvent");
-    })
-    //Take last event(latest) and get commits array;
-    .then( (data)=> {
-        //take first array which is newest and get url to data
-        let latestCommits = data[0].payload.commits;
+    // Take JSON and take only "PushEvents" aka push to origin
+    .then( data => {
+        let pushEvents = data.filter(item => item.type==="PushEvent");
+        // Take first event(latest) and get commits array;
+        let latestCommits = pushEvents[0].payload.commits;
+        // Take first array which is newest and get url to data
         let latest = latestCommits[latestCommits.length-1].url;
         //use url of commit to fetch commit data
-        fetch(latest).then(response => {
-            return response.json()
-        })
-            .then( (data) =>{
-                //get date from data
-                let dateData = data.commit.committer.date;
-                //convert to date
-                $("#latestCommit").html( "<p>Your latest commit was : </p>" + new Date(dateData) );
-            })
-    } )
+        return fetch(latest);
+    })
+    // Take data and turn to JSON
+    .then(response => {
+        return response.json()
+    })
+    // Use JSON data to get information
+    .then( (data) => {
+        //get date from data
+        let dateData = data.commit.committer.date;
+        //convert to date and add to HTML latest commit
+        $("#latestCommit").html("<p>Your latest commit was : </p>" + new Date(dateData));
+    })
+    // If any errors or fails add to HTML error message
     .catch( error => $("#latestCommit").html("<p> Something went wrong. Check the username and try again!</p>"));
 
+// On button click take input data and use as username
 $("#usernameBtn").click(function(){
     let username = $("#username").val();
     githubData(username);
+    // Rest input
     $("#username").val("");
 });
 
